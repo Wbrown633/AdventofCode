b4209da205111fd2e23ee623586bfe03e87499ad
Will Brown <wbrown@starry.com>
2020-12-24 14:25:14 -0500

    Adding more docstrings for Solution Day 5

--- Day 5/day5.py
+++ Day 5/day5.py
@@ -1,6 +1,69 @@
 
-def find_seat(instruction_string):
-    pass
+def max_seat_id_of_inst_list(input_file: str):
+    """Calculate the seat IDs for all the instruction stings in the input file. Then return
+    the max calculated seat ID."""
+    
+    f = open(input_file, "r")
+    seat_ids = []
+    for instruction_string in f:
+        string = instruction_string.strip("\n")
+        seat_ids.append(find_seat_id(string))
+    f.close()
+    
+    missing_id = find_missing_seat_id(seat_ids)
+    print("Your Seat is: ", missing_id)
+    return max(seat_ids)
+
+def find_missing_seat_id(seat_ids: list):
+    """For the second half of the prompt we are told to look for our seat. The prompt states that
+    the entire plane is booked, and so our seat should the only seat ID that is missing from the list.
+    To find the seat ID that's missing we first sort the incoming list of seat IDs that we calculated
+    Then we construct a list of all valid seat IDs. After that we zip the list of all possible seat 
+    ID's together with the list of seat IDs we've calculated. As we traverse the list at somepoint 
+    our list of calculated seat IDs will be missing a value and will therefore skip past the value
+    in the list of all possible seat IDs. At that moment we know that the value in the list of all
+    possible seat IDs is an empty seat (because it doesn't appear in our list of seat IDs) and since
+    we know that our seat is the only empty seat, we know we have found our seat and can stop searching."""
+    
+    seat_ids.sort()
+    all_seats = range(seat_ids[0], seat_ids[-1])
+    both_lists = zip(all_seats, seat_ids)
+    for all_seat, seat_id in both_lists:
+        if all_seat != seat_id:
+            return all_seat
+    
+    raise ValueError("We searched the whole plane and couldn't find your seat!")
+
+def find_seat_id(instruction_string):
+    row_string = instruction_string[:7]
+    column_string = instruction_string[7:]
+    
+    row = find_row(row_string)
+    column = find_column(column_string)
+
+    seat_id = row * 8 + column
+    # print("Row: ", row, "Column: ", column)
+    # print("Instruction: ", instruction_string, "Seat", seat_id )
+
+    return seat_id
+
+def find_row(instruction_string):
+    """Iterate through all of the instructions in the given instruction string and
+    return the row for the given seat as an int."""
+    row_range = (0, 127)
+    for instruction in instruction_string:
+        row_range = binary_split_rows(row_range, instruction)
+    
+    return row_range
+
+def find_column(instruction_string):
+    """Iterate through all of the instructions in the given instruction string and 
+    return the proper column for the seat as an int."""
+    column_range = (0, 7)
+    for instruction in instruction_string:
+        column_range = binary_split_columns(column_range, instruction)
+    
+    return column_range
 
 def binary_split_rows(range_tuple, half):
     """Given a tuple representing the range of remaining options and the half
@@ -14,7 +77,7 @@ def binary_split_rows(range_tuple, half):
     elif half == "F":
         ishigh = False
     else:
-        raise ValueError("Invalid half marker, must be one of {F, B}")
+        raise ValueError("Invalid half marker, must be one of {F, B}, but was: ", half)
 
     return binary_split(range_tuple, ishigh)
 
@@ -28,7 +91,7 @@ def binary_split(range_tuple, ishigh):
         else:
             return lower
     
-    midpoint = higher // 2  # integer division, floors the result
+    midpoint = (higher + lower) // 2  # integer division, floors the result
 
     if ishigh:
         return (midpoint + 1, higher)      
@@ -41,6 +104,9 @@ def binary_split_columns(range_tuple, half):
     elif half == "L":
         ishigh = False
     else:
-        raise ValueError("Invalid half marker, must be one of {L, R}")
+        raise ValueError("Invalid half marker, must be one of {L, R}, but was: ", half)
 
     return binary_split(range_tuple, ishigh)
+
+if __name__ == "__main__":
+    print(max_seat_id_of_inst_list("input.txt"))
--- Day 5/day5_test.py
+++ Day 5/day5_test.py
@@ -1,5 +1,64 @@
 import unittest
+from day5 import binary_split_columns, binary_split_rows, find_seat_id, find_column, find_row
 
 
 class testDay5(unittest.TestCase):
-    pass
+        
+    def test_binary_split_columns(self):
+        """
+        INPUT: RLR
+        Start by considering the whole range, columns 0 through 7.
+        R means to take the upper half, keeping columns 4 through 7.
+        L means to take the lower half, keeping columns 4 through 5.
+        The final R keeps the upper of the two, column 5."""
+        
+        self.assertEqual(binary_split_columns((0, 7), "R"), (4, 7))
+        self.assertEqual(binary_split_columns((4, 7), "L"), (4, 5))
+        self.assertEqual(binary_split_columns((4, 5), "R"), 5)
+
+    def test_binary_split_rows(self):
+        """
+        INPUT: FBFBBFF
+        Start by considering the whole range, rows 0 through 127.
+        F means to take the lower half, keeping rows 0 through 63.
+        B means to take the upper half, keeping rows 32 through 63.
+        F means to take the lower half, keeping rows 32 through 47.
+        B means to take the upper half, keeping rows 40 through 47.
+        B keeps rows 44 through 47.
+        F keeps rows 44 through 45.
+        The final F keeps the lower of the two, row 44."""
+
+        self.assertEqual(binary_split_rows((0, 127), "F"), (0, 63))
+        self.assertEqual(binary_split_rows((0, 63), "B"), (32, 63))
+        self.assertEqual(binary_split_rows((32, 63), "F"), (32, 47))
+        self.assertEqual(binary_split_rows((32, 47), "B"), (40, 47))
+        self.assertEqual(binary_split_rows((40, 47), "B"), (44, 47))
+        self.assertEqual(binary_split_rows((44, 47), "F"), (44, 45))
+        self.assertEqual(binary_split_rows((44, 45), "F"), 44)
+
+    def test_find_column(self):
+        self.assertEqual(find_column("RLR"), 5)
+        self.assertEqual(find_column("RRR"), 7)
+        self.assertEqual(find_column("RLL"), 4)
+
+    def test_find_row(self):
+        self.assertEqual(find_row("FBFBBFF"), 44)
+        self.assertEqual(find_row("BFFFBBF"), 70)
+        self.assertEqual(find_row("FFFBBBF"), 14)
+        self.assertEqual(find_row("BBFFBBF"), 102)
+
+    def test_find_seat_id(self):
+        """
+        FBFBBFFRLR: row 44, column 5, seat ID 357
+        BFFFBBFRRR: row 70, column 7, seat ID 567.
+        FFFBBBFRRR: row 14, column 7, seat ID 119.
+        BBFFBBFRLL: row 102, column 4, seat ID 820"""
+
+        self.assertEqual(find_seat_id("FBFBBFFRLR"), 357)
+        self.assertEqual(find_seat_id("BFFFBBFRRR"), 567)
+        self.assertEqual(find_seat_id("FFFBBBFRRR"), 119)
+        self.assertEqual(find_seat_id("BBFFBBFRLL"), 820)
+
+
+if __name__ == "__main__":
+    unittest.main()
