7deb6183a4cad4a550bd6aad8473a50c662f675d
Will Brown <wbrown@starry.com>
2020-12-26 11:08:03 -0500

    Progress on Day 6 part 2, still a bug in multiple groups

--- Day 6/Day6.py
+++ Day 6/Day6.py
@@ -1,4 +1,4 @@
-def parse_form(file: str):
+def parse_form(file: str, all_yes: bool):
     """
     Given a text file representing the puzzle input, return the sum of unique
     letters per grouping. Group answers are seperated by a blank line.
@@ -7,15 +7,50 @@ def parse_form(file: str):
     groups = create_groups(f.read())
     sum = 0
     for group in groups:
-        sum += parse_form_group(group)
+        if all_yes:    
+            sum += parse_form_group_all_yes(group)
+        else:
+            sum += parse_form_group(group)
 
     return sum
 
 def create_groups(file_contents: str):
     return file_contents.split("\n\n")
 
-def parse_form_group_all_yes(file_contents: str):
-    pass
+def parse_form_group_all_yes(group_contents: str) -> int:
+    """Given the answers from one group, return a count of questions for which
+    ALL group memebers answered YES. Practically this means the letters that appear
+    in all lines of the input."""
+
+    letter_counts = {}
+    all_answered = []
+    num_of_group_members = 0
+    sum_of_answers = 0
+
+    # Count how many times each letter occurs in the group input, including newline
+    for char in group_contents:
+        if char not in letter_counts.keys():
+            letter_counts[char] = 1
+        else:
+            letter_counts[char] += 1
+
+    print(letter_counts)
+    num_of_group_members = find_num_of_group_members(letter_counts)
+
+    for char in letter_counts.keys():
+        if letter_counts[char] == num_of_group_members:
+            print("Letter: ", char, "Add: ", letter_counts[char])
+            sum_of_answers += letter_counts[char]
+
+    return sum_of_answers
+
+def find_num_of_group_members(answers: dict) -> int:
+    """Identify the number of group members (# of '\n' chars + 1)"""
+
+    if "\n" in answers.keys():
+        return answers["\n"] + 1
+    else:
+        return 1
 
 def parse_form_group(answers: str) -> int:
     """Given the answers from one group, return a count of questions people 
@@ -30,4 +65,5 @@ def parse_form_group(answers: str) -> int:
 
 
 if __name__ == "__main__":
-    print("Total sum of group answers:", parse_form("input.txt"))
+    print("Total sum of group answers:", parse_form("input.txt", False))
+    print("Total sum of all group yes: ", parse_form("input.txt", True))
--- Day 6/Day6_test.py
+++ Day 6/Day6_test.py
@@ -1,12 +1,15 @@
 import unittest
-from Day6 import parse_form, parse_form_group
+from Day6 import parse_form, parse_form_group, parse_form_group_all_yes
 
 class TestCase(unittest.TestCase):
     def test_parse_form(self):
         """
         docstring
         """
-        self.assertEqual(parse_form("test_input.txt"), 11)
+        self.assertEqual(parse_form("test_input.txt", False), 11)
+
+    def test_parse_form_all_yes(self):
+        self.assertEqual(parse_form("test_input.txt", True), 6)
 
     def test_parse_form_group(self):
         """
@@ -15,6 +18,8 @@ class TestCase(unittest.TestCase):
         self.assertEqual(parse_form_group("abcx\nabcy\nabcz\n"), 6)
 
     def test_parse_form_group_all_yes(self):
+        self.assertEqual(parse_form_group_all_yes("a\na\na\na"), 4)
         self.assertEqual(parse_form_group_all_yes("abc"), 3)
+        
 
     
